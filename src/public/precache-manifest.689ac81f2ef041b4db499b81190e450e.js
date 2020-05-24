be0320eecf70105021fc616154d78e356d2f2385
alejo1026 <dahdhenao@gmail.com>
2020-05-24 16:38:06 -0500

    preparandoe l servidor de la api rest

---
+++ src/public/precache-manifest.689ac81f2ef041b4db499b81190e450e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d3ccfc03b9ff1242918",
+    "url": "/css/chunk-vendors.94bc0478.css"
+  },
+  {
+    "revision": "da286a24258b2eea3daa874924671800",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4d91ef65f64e529f474",
+    "url": "/js/app.1f450098.js"
+  },
+  {
+    "revision": "8d3ccfc03b9ff1242918",
+    "url": "/js/chunk-vendors.dccaf10c.js"
+  },
+  {
+    "revision": "c41105664e8db6ef494f29d2c35fbf43",
+    "url": "/manifest.json"
+  }
+]);
---
+++ src/server.js
@@ -0,0 +1,31 @@
+const cors = require('cors'); 
+const express = require('express');
+const morgan = require('morgan')
+const path = require('path');
+const app = express();
+
+ 
+
+
+
+//midelwares
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+//midelware para vuejs reouter modo history
+const history = require('connect-history-api-fallback')
+app.use(history())
+app.use(express.static(path.join(__dirname,'public')));
+
+
+const host = process.env.HOST || '0.0.0.0';
+const port  = process.env.PORT || 7500
+
+//app.set('port', process.env.PORT || 9000);
+
+app.listen(port,host,() => {
+    console.log('server on port',port);
+});
